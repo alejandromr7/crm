51e5058c59717926825cb8b146240d8ba3f2a919
Alex <alejandro_mr7@outlook.com>
2022-05-25 21:28:09 -0500

    Creando las transacciones get, post, put,delete

--- controllers/clientesController.js
+++ controllers/clientesController.js
@@ -1,15 +1,67 @@
-
+const Clientes = require("../models/Clientes");
 
 const consultarClientes = async (req, res) => {
-    res.json('Clientes');
+    const cliente = await Clientes.findOne({ where: { email: req.body.email } });
+    res.json(cliente);
 }
 
 const crearCliente = async (req, res) => {
-    res.json('Cliente guardado');
+    const cliente = await Clientes.findOne({ where: { email: req.body.email } });
+
+    if (cliente) {
+        res.json({ msg: 'Este cliente ya se encuentra registrado', error: true });
+        return;
+    }
+    await Clientes.create(req.body);
+    res.json({ msg: 'Registrado correctamente', error: false });
 }
 
+const actualizarCliente = async (req, res) => {
+
+    const { id } = req.params;
+    const { nombre, email, empresa, telefono, notas } = req.body;
+    const cliente = await Clientes.findByPk(id)
+
+    if (!cliente) {
+        res.json({ msg: 'No hay coincidencias para este cliente', error: true });
+        return;
+    }
+
+    try {
+        cliente.nombre = nombre;
+        cliente.email = email;
+        cliente.empresa = empresa;
+        cliente.telefono = telefono;
+        cliente.notas = notas;
+        await cliente.save();
+
+        res.json({ msg: 'Cliente actualizado correctamente', error: false });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const eliminarCliente = async (req, res) => {
+
+    const { id } = req.params;
+    const cliente = await Clientes.findByPk(id)
+
+    if (!cliente) {
+        res.json({ msg: 'No hay coincidencias para este cliente', error: true });
+        return;
+    }
+
+    try {
+        await cliente.destroy()
+        res.json({ msg: 'Cliente eliminado correctamente', error: false });
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 module.exports = {
     consultarClientes,
-    crearCliente
+    crearCliente,
+    actualizarCliente,
+    eliminarCliente
 }
--- routes/clientesRoutes.js
+++ routes/clientesRoutes.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
-const { consultarClientes, crearCliente } = require('../controllers/clientesController');
+const { consultarClientes, crearCliente, actualizarCliente, eliminarCliente } = require('../controllers/clientesController');
 const router = Router();
 
 router.route('/').get(consultarClientes).post(crearCliente);
+router.route('/:id').put(actualizarCliente).delete(eliminarCliente);
 
 module.exports = router;
